d9194a91cf575b58bb8f19a610e44ebe77710068
Belin27000 <y.lecerf@protonmail.com>
2025-02-05 08:19:32 +0100

    Configure keypair with .env variable

--- generate-keypair.ts
+++ generate-keypair.ts
@@ -1,6 +1,13 @@
 import { Keypair } from "@solana/web3.js"
-const keypair = Keypair.generate();
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers"
+
+//const keypair = Keypair.generate();
 console.log(`✅Generate keypair`);
-console.log(`The public key is: `, keypair.publicKey.toBase58());
-console.log(`The secret key is: `, keypair.secretKey);
-console.log(`✅ Finished!`);
+//console.log(`The public key is: `, keypair.publicKey.toBase58());
+//console.log(`The secret key is: `, keypair.secretKey);
+//console.log(`✅ Finished!`);
+const keypairFromEnv = Keypair.fromSecretKey(
+    Uint8Array.from(JSON.parse(process.env.SECRET_KEY || "[]"))
+);
+console.log("✅ Keypair loaded successfully!", keypairFromEnv.publicKey.toBase58());
